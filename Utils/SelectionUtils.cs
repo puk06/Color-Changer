3994ba5bd3f413dadc10f323b5cb178e8d8c6645
Puk06 <86549420+puk06@users.noreply.github.com>
2025-07-29 11:34:15 +0900

    コメントの追加

--- Utils/SelectionUtils.cs
+++ Utils/SelectionUtils.cs
@@ -4,6 +4,11 @@ internal static class SelectionUtils
 {
     private static readonly SolidBrush _selectionOverlayBrush = new SolidBrush(Color.FromArgb(150, 255, 0, 0));
 
+    /// <summary>
+    /// 渡されたGraphicsに選択ペンのリアルタイムプレビューを描画します
+    /// </summary>
+    /// <param name="graphics"></param>
+    /// <param name="previewMap"></param>
     internal static void SetSelectionPreviewMap(Graphics graphics, bool[,] previewMap)
     {
         int width = previewMap.GetLength(0);
